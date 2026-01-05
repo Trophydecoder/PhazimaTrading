4c5e106ce21bd9b97c3a8b56962b04a58d3079a0
Trophydecoder <trophydecoder@gmail.com>
2026-01-05 13:24:54 +0200

    nav done

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { LandingComponent } from "../../components/landing/landing.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, LandingComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
