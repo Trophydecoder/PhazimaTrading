3ecdeff9d9b5e122380ca6dcfddfc6f54bafbf19
Trophydecoder <trophydecoder@gmail.com>
2026-01-06 03:38:16 +0200

    call us component done

---
+++ src/app/components/callus/callus.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-callus',
+  imports: [],
+  templateUrl: './callus.component.html',
+  styleUrl: './callus.component.scss'
+})
+export class CallusComponent {
+
+}
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -3,10 +3,11 @@ import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { LandingComponent } from "../../components/landing/landing.component";
 import { PromotionalpointsComponent } from "../../components/promotionalpoints/promotionalpoints.component";
 import { AboutusComponent } from "./../../components/aboutus/aboutus.component";
+import { CallusComponent } from "../../components/callus/callus.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [NavbarComponent, LandingComponent, PromotionalpointsComponent, AboutusComponent],
+  imports: [NavbarComponent, LandingComponent, PromotionalpointsComponent, AboutusComponent, CallusComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
