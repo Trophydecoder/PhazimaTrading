d8681e883e58821be1edd26df4ca81833df8c193
Trophydecoder <trophydecoder@gmail.com>
2026-01-06 03:10:26 +0200

    aboutus

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { LandingComponent } from "../../components/landing/landing.component";
 import { PromotionalpointsComponent } from "../../components/promotionalpoints/promotionalpoints.component";
+import { AboutusComponent } from "./../../components/aboutus/aboutus.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [NavbarComponent, LandingComponent, PromotionalpointsComponent],
+  imports: [NavbarComponent, LandingComponent, PromotionalpointsComponent, AboutusComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
