5ffcdde17ddc47613710b5b55fd1215dcd8b6907
Trophydecoder <trophydecoder@gmail.com>
2026-01-13 22:16:44 +0200

    added a component our mission

--- src/app/pages/aboutpage/aboutpage.component.ts
+++ src/app/pages/aboutpage/aboutpage.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { AboutusLandingComponent } from "../../components/aboutus-landing/aboutus-landing.component";
 import { AboutusComponent } from "../../components/aboutus/aboutus.component";
+import { AboutMissionComponent } from "../../components/about-mission/about-mission.component";
 
 @Component({
   selector: 'app-aboutpage',
-  imports: [NavbarComponent, AboutusLandingComponent, AboutusComponent],
+  imports: [NavbarComponent, AboutusLandingComponent, AboutusComponent, AboutMissionComponent],
   templateUrl: './aboutpage.component.html',
   styleUrl: './aboutpage.component.scss'
 })
